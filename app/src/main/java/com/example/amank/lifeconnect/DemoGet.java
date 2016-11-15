849a06523bc564313f0e77afa9508df59ff2e38d
Abhishek-P-Joshi <abhijosh92@gmail.com>
2016-11-14 19:30:46 -0700

    Seperate class to get the Data from the database

--- app/src/main/java/com/example/amank/lifeconnect/DemoGet.java
+++ app/src/main/java/com/example/amank/lifeconnect/DemoGet.java
@@ -1,24 +1,15 @@
 package com.example.amank.lifeconnect;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -37,6 +28,7 @@ public class DemoGet extends AppCompatActivity {
     ArrayList<HashMap<String, String>> personList;
 
     ListView list;
+    private String JsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +37,14 @@ public class DemoGet extends AppCompatActivity {
 
         list = (ListView) findViewById(R.id.listView);
         personList = new ArrayList<HashMap<String,String>>();
-        getData();
+        String sql = "SELECT * FROM Demo";
+        GetFromDatabase getFromDatabase = new GetFromDatabase();
+        JsonString = getFromDatabase.GetData(sql, FileName.ServerPHP.Demo);
+        //getData(sql);
+        showList(JsonString);
     }
 
-    protected void showList(){
+    protected void showList(String myJSON){
         try {
             JSONObject jsonObj = new JSONObject(myJSON);
             peoples = jsonObj.getJSONArray(TAG_RESULTS);
@@ -84,51 +80,59 @@ public class DemoGet extends AppCompatActivity {
 
     }
 
-    public void getData(){
-        class GetDataJSON extends AsyncTask<String, Void, String> {
-
-            @Override
-            protected String doInBackground(String... params) {
-                DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
-                HttpPost httppost = new HttpPost("http://79.170.40.227/lifeconnect.com/DemoGet.php");
-
-                // Depends on your web service
-                httppost.setHeader("Content-type", "application/json");
-
-                InputStream inputStream = null;
-                String result = null;
-                try {
-                    HttpResponse response = httpclient.execute(httppost);
-                    HttpEntity entity = response.getEntity();
-
-                    inputStream = entity.getContent();
-                    // json is UTF-8 by default
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
-                    StringBuilder sb = new StringBuilder();
-
-                    String line = null;
-                    while ((line = reader.readLine()) != null)
-                    {
-                        sb.append(line + "\n");
-                    }
-                    result = sb.toString();
-                } catch (Exception e) {
-                    // Oops
-                }
-                finally {
-                    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
-                }
-                return result;
-            }
-
-            @Override
-            protected void onPostExecute(String result){
-                myJSON=result;
-                showList();
-            }
-        }
-        GetDataJSON g = new GetDataJSON();
-        g.execute();
-    }
+//    public void getData(final String sql){
+//        class GetDataJSON extends AsyncTask<String, Void, String> {
+//
+//            @Override
+//            protected String doInBackground(String... params) {
+//
+//                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+//                nameValuePairs.add(new BasicNameValuePair("sql", sql));
+//
+//
+//                InputStream inputStream = null;
+//                String result = null;
+//                try {
+//                    DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+//                    HttpPost httppost = new HttpPost("http://79.170.40.227/lifeconnect.com/DemoGet.php");
+//                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//
+//
+//                    // Depends on your web service
+//                    //httppost.setHeader("Content-type", "application/json");
+//
+//                    HttpResponse response = httpclient.execute(httppost);
+//                    HttpEntity entity = response.getEntity();
+//
+//                    inputStream = entity.getContent();
+//                    // json is UTF-8 by default
+//                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+//                    StringBuilder sb = new StringBuilder();
+//
+//                    String line = null;
+//                    while ((line = reader.readLine()) != null)
+//                    {
+//                        sb.append(line + "\n");
+//                    }
+//                    result = sb.toString();
+//                } catch (Exception e) {
+//                    // Oops
+//                }
+//                finally {
+//                    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+//                }
+//                return result;
+//            }
+//
+//            @Override
+//            protected void onPostExecute(String result){
+//                myJSON=result;
+//                showList(myJSON);
+//                //return myJSON;
+//            }
+//        }
+//        GetDataJSON g = new GetDataJSON();
+//        g.execute();
+//    }
 
 }
---
+++ app/src/main/java/com/example/amank/lifeconnect/FileName.java
@@ -0,0 +1,21 @@
+package com.example.amank.lifeconnect;
+
+/**
+ * Created by Abhishek on 11/14/2016.
+ */
+
+public final class FileName {
+
+    private FileName(){
+
+    }
+
+    public final class ServerPHP {
+        public static final String Demo = "Demo";
+        public static final String Patient = "Patient";
+        public static final String Doctor = "Doctor";
+
+        private ServerPHP(){}
+    }
+
+}
