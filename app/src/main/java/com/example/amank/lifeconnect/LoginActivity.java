27f6d9e458636c6f7830b3f7fac47639748a26be
Aman Karnik <aman.karnik@gmail.com>
2016-11-22 00:52:43 -0700

    Final final login activity

--- app/src/main/java/com/example/amank/lifeconnect/LoginActivity.java
+++ app/src/main/java/com/example/amank/lifeconnect/LoginActivity.java
@@ -5,12 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +25,9 @@ public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
+    RadioGroup radioButtonGroup;
+    RadioButton r;
+    String userselectText;
     //private static final String TAG_PASSWORD = "Password";
     //private static final String TAG_NAME = "Name";
     private static final String TAG_RESULTS="result";
@@ -47,6 +51,7 @@ public class LoginActivity extends AppCompatActivity {
 //        setSupportActionBar(toolbar);
 
         ButterKnife.inject(this);
+        radioButtonGroup = (RadioGroup) findViewById(R.id.user_selection);
         ActivityCompat.requestPermissions(this, new String[] {
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION },1);
@@ -60,37 +65,35 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    public void login(String username, String password) {
+    public void login(String username, String password, String userSelection) {
         //boolean flag = false;
-        Log.d(TAG, "Login");
-        String sql = "SELECT Name,Password FROM Demo WHERE Name ='" + username + "' AND Password ='" + password+"'";
-        GetFromDatabase getFromDatabase = new GetFromDatabase();
-        authenticate = getFromDatabase.GetData(sql, FileName.ServerPHP.Demo);
-        try {
-            JSONObject jsonObj = new JSONObject(authenticate);
-            values = jsonObj.getJSONArray(TAG_RESULTS);
-            if(values.length()>0)
-            {
-                Intent startSenseService = new Intent(LoginActivity.this, SensorHandler.class);
-                Bundle b = new Bundle();
-                b.putString("name", username);
-                startSenseService.putExtras(b);
-                startService(startSenseService);
-                //flag = true;
-                Intent intent = new Intent(this, DemoInsert.class);
-                startActivity(intent);
-            }else{
-                Toast.makeText(getBaseContext(),"Please Enter Proper Credentials!",Toast.LENGTH_SHORT).show();
-                onLoginFailed();
-            }
+        //Log.d(TAG, "Login");
+        if (userSelection == "Doctor") {
+            String sql = "SELECT Name,Password FROM Doctor WHERE Name ='" + username + "' AND Password ='" + password + "'";
+            GetFromDatabase getFromDatabase = new GetFromDatabase();
+            authenticate = getFromDatabase.GetData(sql, FileName.ServerPHP.Demo);
+            try {
+                JSONObject jsonObj = new JSONObject(authenticate);
+                values = jsonObj.getJSONArray(TAG_RESULTS);
+                if (values.length() > 0) {
+                    //flag = true;
+                    Intent intent = new Intent(this, DemoInsert.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getBaseContext(), "Please Enter Proper Credentials!", Toast.LENGTH_SHORT).show();
+                    onLoginFailed();
+                }
 //            JSONObject c = values.getJSONObject(0);
 //            String name = c.getString(TAG_NAME);
 //            String passwd = c.getString(TAG_PASSWORD);
-        }catch(Exception e){
+            } catch (Exception e) {
+                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
 
-        }
-
-        _loginButton.setEnabled(true);
+//            JSONObject c = values.getJSONObject(0);
+//            String name = c.getString(TAG_NAME);
+//            String passwd = c.getString(TAG_PASSWORD)
+            _loginButton.setEnabled(true);
 
 //        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
 //                R.style.AppTheme_Dark_Dialog);
@@ -98,8 +101,9 @@ public class LoginActivity extends AppCompatActivity {
 //        progressDialog.setMessage("Authenticating...");
 //        progressDialog.show();
 
-        //String email = _emailText.getText().toString();
-        //String password = _passwordText.getText().toString();
+            //String email = _emailText.getText().toString();
+            //String password = _passwordText.getText().toString();
+        }
     }
 
     public void onLoginFailed() {
@@ -116,6 +120,10 @@ public class LoginActivity extends AppCompatActivity {
 
     public void validate() {
         boolean valid = true;
+        int radioButtonID = radioButtonGroup.getCheckedRadioButtonId();
+        View radioButton = radioButtonGroup.findViewById(radioButtonID);
+        int idx = radioButtonGroup.indexOfChild(radioButton);
+        r = (RadioButton)radioButtonGroup.getChildAt(idx);
 
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
@@ -133,8 +141,16 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             _passwordText.setError(null);
         }
+
+        if (idx < 0) {
+            Toast.makeText(LoginActivity.this, "Please select an option", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else{
+            userselectText = r.getText().toString();
+        }
         if(valid == true){
-            login(email,password);
+            login(email,password,userselectText);
         }else{
             onLoginFailed();
         }
@@ -160,7 +176,6 @@ public class LoginActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
