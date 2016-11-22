0a58122354e7ba7219c46f8c8e45466293bcc3b4
Aman Karnik <aman.karnik@gmail.com>
2016-11-22 12:31:15 -0700

    last again

--- app/src/main/java/com/example/amank/lifeconnect/LoginActivity.java
+++ app/src/main/java/com/example/amank/lifeconnect/LoginActivity.java
@@ -176,14 +176,14 @@ public class LoginActivity extends AppCompatActivity {
         String password = _passwordText.getText().toString();
 
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
+            _emailText.setError("Enter a valid email address");
             valid = false;
         } else {
             _emailText.setError(null);
         }
 
         if (password.isEmpty() || password.length() < 4 || password.length() > 12) {
-            _passwordText.setError("between 4 and 12 alphanumeric characters");
+            _passwordText.setError("Password between 4 and 12 alphanumeric characters");
             valid = false;
         } else {
             _passwordText.setError(null);
