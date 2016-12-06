ec5877071b091b7456aec59e8be29e0b55d99084
YogeshKubal <ymkubal92@gmail.com>
2016-12-05 17:42:57 -0700

    Face recognition included

---
+++ app/src/main/java/com/example/amank/lifeconnect/UploadRegistration.java
@@ -0,0 +1,196 @@
+package com.example.amank.lifeconnect;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.kairos.Kairos;
+import com.kairos.KairosListener;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Hashtable;
+import java.util.Map;
+
+import com.kairos.*;
+
+import org.json.JSONException;
+
+/**
+ * Created by Yogesh on 12/05/2016.
+ */
+
+public class UploadRegistration extends CaptureRegistration implements View.OnClickListener{
+    private Button buttonChoose;
+    private Button buttonUpload;
+    private ImageView imageView;
+
+    private Bitmap bitmap;
+
+    private int PICK_IMAGE_REQUEST = 1;
+
+    private String UPLOAD_URL ="http://79.170.40.227/lifeconnect.com/VolleyUpload/upload.php";
+
+    private String KEY_IMAGE = "image";
+    private String KEY_NAME = "name";
+
+    private String ENROLL_URL = "http://79.170.40.227/lifeconnect.com/VolleyUpload/upload/yogesh_1.jpg";
+    private String galleryId = "Doctor";
+    private Kairos myKairos;
+    private KairosListener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_registration);
+
+        buttonChoose = (Button) findViewById(R.id.buttonChooseRegister);
+        buttonUpload = (Button) findViewById(R.id.buttonUploadRegister);
+
+        imageView  = (ImageView) findViewById(R.id.imageViewRegister);
+
+        buttonChoose.setOnClickListener(this);
+        buttonUpload.setOnClickListener(this);
+
+        /* * * instantiate a new kairos instance * * */
+        myKairos = new Kairos();
+        /* * * set authentication * * */
+        String app_id = "ce940a3c";
+        String api_key = "21e45f6b99393ef067823b4ee24149a5";
+        myKairos.setAuthentication(this, app_id, api_key);
+
+        // listener
+        listener = new KairosListener() {
+
+            @Override
+            public void onSuccess(String response) {
+                Log.d("KAIROS DEMO", response);
+            }
+
+            @Override
+            public void onFail(String response) {
+                Log.d("KAIROS DEMO", response);
+            }
+        };
+    }
+
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void uploadImage(){
+        //Showing the progress dialog
+        final ProgressDialog loading = ProgressDialog.show(this,"Uploading...","Please wait...",false,false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //Disimissing the progress dialog
+                        loading.dismiss();
+                        //Showing toast message of the response
+                        Toast.makeText(UploadRegistration.this, "Registration complete" , Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        //Dismissing the progress dialog
+                        loading.dismiss();
+
+                        //Showing toast
+                        Toast.makeText(UploadRegistration.this, volleyError.getMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Converting Bitmap to String
+                String image = getStringImage(bitmap);
+
+                //Creating parameters
+                Map<String,String> params = new Hashtable<String, String>();
+
+                //Adding parameters
+                params.put(KEY_IMAGE, image);
+
+                //returning parameters
+                return params;
+            }
+        };
+
+        //Creating a Request Queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //Adding request to the queue
+        requestQueue.add(stringRequest);
+
+        String image = ENROLL_URL;
+        String subjectId = "Yogesh";
+        try {
+            myKairos.enroll(image, subjectId, galleryId, null, null, null, listener);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(UploadRegistration.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //Getting the Bitmap from Gallery
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //Setting the Bitmap to ImageView
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == buttonChoose){
+            showFileChooser();
+        }
+
+        if(v == buttonUpload){
+            uploadImage();
+        }
+    }
+}
